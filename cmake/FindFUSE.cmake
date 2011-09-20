d5107060b7a07b4606e9de909eeeccd71290c9d4
Bernd L?rwald <bloerwald@googlemail.com>
2011-09-21 00:33:17 +0200

    Added cmake module for finding fuse.

---
+++ cmake/FindFUSE.cmake
@@ -0,0 +1,24 @@
+FIND_PACKAGE(PkgConfig)
+PKG_CHECK_MODULES(PC_FUSE fuse)
+
+FIND_PATH( FUSE_INCLUDE_DIR fuse/fuse.h
+           HINTS
+           ${PC_FUSE_INCLUDE_DIRS}
+           ${PC_FUSE_INCLUDEDIR}
+           PATH_SUFFIXES fuse
+          )
+
+FOREACH (lib ${PC_FUSE_LIBRARIES})
+  FIND_LIBRARY( FUSE_LIBRARY_${lib} NAMES ${lib}
+                HINTS
+                ${PC_FUSE_LIBDIR}
+                ${PC_FUSE_LIBRARY_DIRS}
+        )
+
+  SET(FUSE_LIBRARIES ${FUSE_LIBRARIES} ${FUSE_LIBRARY_${lib}})
+ENDFOREACH (lib ${PC_FUSE_LIBRARIES})
+
+if (NOT FUSE_INCLUDE_DIR)
+  message (FATAL_ERROR "FUSE NOT FOUND!")
+endif (NOT FUSE_INCLUDE_DIR)
+
